628cf244310883c82956a0f8045101655fd462ef
Vincent Noël <contact@vincent-noel.fr>
2019-05-29 11:27:13 +0200

    First draft of travis testing

--- engine/src/init-config.sh
+++ engine/src/init-config.sh
@@ -12,50 +12,50 @@ CONFIG_H=${CONFIG_NAME}.h
 
 if [ $# = 0 -a -r $CONFIG_H ]; then exit 0; fi
 
-tmpfile=/tmp/${CONFIG_NAME}$$.c
+# tmpfile=/tmp/${CONFIG_NAME}$$.c
 
-cat > $tmpfile <<EOF
-#include <unordered_map>
-#include <stdlib.h>
-int main()
-{
-  return 0;
-}
-EOF
+# cat > $tmpfile <<EOF
+# #include <unordered_map>
+# #include <stdlib.h>
+# int main()
+# {
+#   return 0;
+# }
+# EOF
 
-$CXX -c $tmpfile -std=c++11 > /dev/null 2>&1
+# $CXX -c $tmpfile -std=c++11 > /dev/null 2>&1
 
-if [ $? = 0 ]
-then
-   echo "// @HAS_UNORDERED_MAP"  >> $CONFIG_H
-   echo "#define HAS_UNORDERED_MAP" >> $CONFIG_H
-   echo "#include <unordered_map>" >> $CONFIG_H
-   echo "#define STATE_MAP std::unordered_map" >> $CONFIG_H
-   echo "#define HASH_STRUCT hash" >> $CONFIG_H
-else
-    cat > $tmpfile <<EOF
-#include <tr1/unordered_map>
-#include <stdlib.h>
-int main()
-{
-  return 0;
-}
-EOF
+# if [ $? = 0 ]
+# then
+#    echo "// @HAS_UNORDERED_MAP"  >> $CONFIG_H
+#    echo "#define HAS_UNORDERED_MAP" >> $CONFIG_H
+#    echo "#include <unordered_map>" >> $CONFIG_H
+#    echo "#define STATE_MAP std::unordered_map" >> $CONFIG_H
+#    echo "#define HASH_STRUCT hash" >> $CONFIG_H
+# else
+#     cat > $tmpfile <<EOF
+# #include <tr1/unordered_map>
+# #include <stdlib.h>
+# int main()
+# {
+#   return 0;
+# }
+# EOF
 
-    $CXX -c $tmpfile > /dev/null 2>&1
-    if [ $? = 0 ]
-    then
-	echo "// @HAS_UNORDERED_MAP"  >> $CONFIG_H
-	echo "#define HAS_UNORDERED_MAP" >> $CONFIG_H
-	echo "#include <tr1/unordered_map>" >> $CONFIG_H
-	echo "#define STATE_MAP std::tr1::unordered_map" >> $CONFIG_H
-	echo "#define HASH_STRUCT std::tr1::hash" >> $CONFIG_H
-    else
+#     $CXX -c $tmpfile > /dev/null 2>&1
+#     if [ $? = 0 ]
+#     then
+# 	echo "// @HAS_UNORDERED_MAP"  >> $CONFIG_H
+# 	echo "#define HAS_UNORDERED_MAP" >> $CONFIG_H
+# 	echo "#include <tr1/unordered_map>" >> $CONFIG_H
+# 	echo "#define STATE_MAP std::tr1::unordered_map" >> $CONFIG_H
+# 	echo "#define HASH_STRUCT std::tr1::hash" >> $CONFIG_H
+#     else
 	echo "//#define HAS_UNORDERED_MAP" >> $CONFIG_H
 	echo "#define STATE_MAP std::map" >> $CONFIG_H
-    fi
-fi
-rm -f $tmpfile ${CONFIG_NAME}*.o
+#     fi
+# fi
+# rm -f $tmpfile ${CONFIG_NAME}*.o
 
 
 
