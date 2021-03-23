06a50b94b1c44d9dad9458dbfed40233e8564390
Vincent Noël <contact@vincent-noel.fr>
2021-03-23 19:00:54 +0100

    Improved engine hierarchy

--- engine/src/FixedPointEngine.h
+++ engine/src/FixedPointEngine.h
@@ -36,66 +36,64 @@
 #############################################################################
 
    Module:
-     EnsembleEngine.h
+     FixedPointEngine.h
 
    Authors:
      Vincent Noel <contact@vincent-noel.fr>
  
    Date:
-     March 2019
+     March 2021
 */
 
-#ifndef _ENSEMBLEENGINE_H_
-#define _ENSEMBLEENGINE_H_
+#ifndef _FIXEDPOINTENGINE_H_
+#define _FIXEDPOINTENGINE_H_
 
 #include <string>
 #include <map>
 #include <vector>
 #include <assert.h>
 
-#include "ProbTrajEngine.h"
+#ifdef MPI_COMPAT
+#include <mpi.h>
+#endif
+
+#include "MetaEngine.h"
 #include "BooleanNetwork.h"
-#include "Cumulator.h"
-#include "RandomGenerator.h"
 #include "RunConfig.h"
+#include "FixedPointDisplayer.h"
 
 struct EnsembleArgWrapper;
 
-class EnsembleEngine : public ProbTrajEngine {
+class FixedPointEngine : public MetaEngine {
 
-  std::vector<Network*> networks;
-  std::vector<Cumulator*> cumulators_per_model; // The final Cumulators for each model
-  std::vector<STATE_MAP<NetworkState_Impl, unsigned int>* > fixpoints_per_model; // The final fixpoints for each model
-  
-  bool save_individual_result; // Do we want to save individual model simulation result
-  bool random_sampling; // Randomly select the number of simulation per model
+protected:
 
-  std::vector<std::vector<unsigned int> > simulation_indices_v; // The list of indices of models to simulate for each thread
-  std::vector<std::vector<Cumulator*> > cumulator_models_v; // The results for each model, by thread
-  std::vector<std::vector<Cumulator*> > cumulators_thread_v; // The results for each model, by model
-  std::vector<std::vector<STATE_MAP<NetworkState_Impl, unsigned int>*> > fixpoints_models_v; // The fixpoints for each model, by thread
-  std::vector<std::vector<STATE_MAP<NetworkState_Impl, unsigned int>*> > fixpoints_threads_v; // The fixpoints for each model, by thread
-
-  std::vector<EnsembleArgWrapper*> arg_wrapper_v;
-  void epilogue();
-  static void* threadWrapper(void *arg);
-  void runThread(Cumulator* cumulator, unsigned int start_count_thread, unsigned int sample_count_thread, RandomGeneratorFactory* randgen_factory, int seed, STATE_MAP<NetworkState_Impl, unsigned int>* fixpoint_map, std::ostream* output_traj, std::vector<unsigned int> simulation_ind, std::vector<Cumulator*> t_models_cumulators, std::vector<STATE_MAP<NetworkState_Impl, unsigned int>* > t_models_fixpoints);
-  void displayIndividualFixpoints(unsigned int model_id, FixedPointDisplayer* fp_displayer) const;
-  void mergeIndividual();
+  STATE_MAP<NetworkState_Impl, unsigned int> fixpoints;
+  std::vector<STATE_MAP<NetworkState_Impl, unsigned int>*> fixpoint_map_v;
+  STATE_MAP<NetworkState_Impl, unsigned int>* mergeFixpointMaps();
+  static void mergePairOfFixpoints(STATE_MAP<NetworkState_Impl, unsigned int>* fixpoints_1, STATE_MAP<NetworkState_Impl, unsigned int>* fixpoints_2);
 
 #ifdef MPI_COMPAT
-  void mergeMPIIndividual(bool pack=true);
+  static void mergePairOfMPIFixpoints(STATE_MAP<NetworkState_Impl, unsigned int>* fixpoints, int world_rank, int dest, int origin, bool pack=true);
+
+  static void MPI_Unpack_Fixpoints(STATE_MAP<NetworkState_Impl, unsigned int>* fp_map, char* buff, unsigned int buff_size);
+  static char* MPI_Pack_Fixpoints(const STATE_MAP<NetworkState_Impl, unsigned int>* fp_map, int dest, unsigned int * buff_size);
+  static void MPI_Send_Fixpoints(const STATE_MAP<NetworkState_Impl, unsigned int>* fp_map, int dest);
+  static void MPI_Recv_Fixpoints(STATE_MAP<NetworkState_Impl, unsigned int>* fp_map, int origin);
+  
 #endif
 
 public:
-  static const std::string VERSION;
 
-  EnsembleEngine(std::vector<Network*> network, RunConfig* runconfig, bool save_individual_result=false, bool random_sampling=false);
+  FixedPointEngine(Network * network, RunConfig* runconfig) : MetaEngine(network, runconfig) {}
 
-  void run(std::ostream* output_traj);
+  bool converges() const {return fixpoints.size() > 0;}
+  const STATE_MAP<NetworkState_Impl, unsigned int>& getFixpoints() const {return fixpoints;}
+  const std::map<unsigned int, std::pair<NetworkState, double> > getFixPointsDists() const;
 
-  void displayIndividual(unsigned int model_id, ProbTrajDisplayer* probtraj_displayer, StatDistDisplayer* statdist_displayer, FixedPointDisplayer* fp_displayer) const;
-  ~EnsembleEngine();
+  // void displayFixpoints(std::ostream& output_fp, bool hexfloat = false) const;
+  void displayFixpoints(FixedPointDisplayer* displayer) const;
+
 };
 
 #endif
